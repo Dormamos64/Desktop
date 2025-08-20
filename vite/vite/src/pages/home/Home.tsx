d68430a3af4bdecd8ce49c81679d61b850f1e91a
Dormamos64 <gabrieloliveraamaral06@gmail.com>
2025-08-20 09:45:23 -0300

    viteProject

---
+++ vite/vite/src/pages/home/Home.tsx
@@ -0,0 +1,37 @@
+
+function Home() {
+  return (
+    <>
+    <div style={{
+            width:"100vw",
+            display:"flex",
+            justifyContent:"center",
+        }}>
+
+    <div style = {{
+            width:"80vw",
+            display:"flex",
+            flexDirection:"column",
+            alignItems:"center",
+        }}>
+
+        <h2>Seja bem vindo</h2>
+        <p>expresse suas opiniões </p>
+            
+    <div style = {{
+            width:"80vw",
+            display:"flex",
+            flexDirection:"column",
+            alignItems:"center",
+        }}>
+
+    <img src="https://i.imgur.com/VpwApCU.png" alt="imagen da pagina Home" width="400px" /> 
+
+    </div>
+    </div>
+    </div>
+    </>
+    )
+}
+
+export default Home
